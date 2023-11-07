1ec264c41bd5c99b888efe889c6171d3fa299e79
KevinMartinHerrera <117857314+KevinMartinHerrera@users.noreply.github.com>
2023-11-07 01:02:54 -0600

    UPDATE

--- render/build.sh
+++ render/build.sh
@@ -11,5 +11,5 @@ python manage.py migrate
 python manage.py loaddata facultades.json
 
 # Agregar un superusuario
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Kevin_Martin', 'correo@ejemplo.com', '12345678')" | python manage.py shell
+#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Kevin_Martin', 'correo@ejemplo.com', '12345678')" | python manage.py shell
 
